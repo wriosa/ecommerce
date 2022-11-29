d49f30c2de6c5b8d82a10c9c095165248e2c76c3
wriosa <wolfgang.ra2@gmail.com>
2022-11-28 22:53:37 -0500

    purchases

--- src/pages/Purchases.jsx
+++ src/pages/Purchases.jsx
@@ -1,15 +1,32 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { getPurchasesThunk } from '../store/slices/purchases.slice';
 
 const Purchases = () => {
     const dispatch = useDispatch();
+    const purchases = useSelector(state => state.purchases)
     useEffect(() => {
         dispatch(getPurchasesThunk())
     }, [])
     return (
         <div>
-
+            <h1>Purchases</h1>
+            <ul>
+                {
+                    purchases.map(purchase =>(
+                        <li>
+                            {purchase.cart.products.map(product=>(
+                                <Link to={`/product/${product.id}`}>
+                                <li>
+                                    {product.title}
+                                </li>
+                                </Link>
+                            ))}
+                        </li>
+                    ))
+                }
+            </ul>
         </div>
     );
 };
